5108338fefda39d6903ea77419aba50799108c5c
Ksistof Vinco <ksistof.vinco@gmail.com>
2024-08-02 10:06:20 +0300

    changed default device count to -1 (None)

--- lib/upgradedb/postgres.2024073100.php
+++ lib/upgradedb/postgres.2024073100.php
@@ -3,8 +3,8 @@
 $this->BeginTrans();
 
 $this->Execute("ALTER TABLE tms_settings ADD COLUMN additional_devices INTEGER");
-$this->Execute("ALTER TABLE tms_settings ALTER COLUMN additional_devices SET DEFAULT 0");
-$this->Execute("UPDATE tms_settings SET additional_devices = 0");
+$this->Execute("ALTER TABLE tms_settings ALTER COLUMN additional_devices SET DEFAULT -1");
+$this->Execute("UPDATE tms_settings SET additional_devices = -1");
 $this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2024073100', 'dbversion_EtaTmsSync'));
 
 $this->CommitTrans();
